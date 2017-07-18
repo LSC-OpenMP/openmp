74839c72ab005a292fde1c5aac6be7ca7dd0bbe3
Hervé Yviquel <hyviquel@gmail.com>
2017-07-18 13:13:29 -0300

    Fix build of spark deviceRTL

--- libomptarget/cmake/Modules/LibomptargetGetDependencies.cmake
+++ libomptarget/cmake/Modules/LibomptargetGetDependencies.cmake
@@ -280,7 +280,7 @@ mark_as_advanced(
 # Looking for sbt...
 ################################################################################
 
-find_program(SBT_EXECUTABLE
+find_program(LIBOMPTARGET_DEP_SBT_EXECUTABLE
   NAMES
     sbt
   HINTS
@@ -293,10 +293,10 @@ find_program(SBT_EXECUTABLE
 )
 
 find_package_handle_standard_args(
-  SBT
+  LIBOMPTARGET_DEP_SBT
   DEFAULT_MSG
-  SBT_EXECUTABLE)
+  LIBOMPTARGET_DEP_SBT_EXECUTABLE)
 
 mark_as_advanced(
-  SBT_EXECUTABLE
+  LIBOMPTARGET_DEP_SBT_EXECUTABLE
 )
