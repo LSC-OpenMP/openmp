e2a3930e577619e052f758ac0619fce810d8aa1e
Ciro Ceissler <ciro.ceissler@gmail.com>
2017-09-18 00:15:00 -0300

    configure aalsdk cmake path

--- libomptarget/cmake/Modules/LibomptargetGetDependencies.cmake
+++ libomptarget/cmake/Modules/LibomptargetGetDependencies.cmake
@@ -13,9 +13,10 @@
 # components of libomptarget. These are the dependencies we have:
 #
 # libelf : required by some targets to handle the ELF files at runtime.
-# libffi : required to launch target kernels given function and argument 
+# libffi : required to launch target kernels given function and argument
 #          pointers.
 # CUDA : required to control offloading to NVIDIA GPUs.
+# Intel HARP: required to control offloading to Intel HARP FPGAs.
 
 include (FindPackageHandleStandardArgs)
 
@@ -47,18 +48,18 @@ find_library (
     /sw/lib
     ENV LIBRARY_PATH
     ENV LD_LIBRARY_PATH)
-    
+
 set(LIBOMPTARGET_DEP_LIBELF_INCLUDE_DIRS ${LIBOMPTARGET_DEP_LIBELF_INCLUDE_DIR})
 find_package_handle_standard_args(
-  LIBOMPTARGET_DEP_LIBELF 
+  LIBOMPTARGET_DEP_LIBELF
   DEFAULT_MSG
   LIBOMPTARGET_DEP_LIBELF_LIBRARIES
   LIBOMPTARGET_DEP_LIBELF_INCLUDE_DIRS)
 
 mark_as_advanced(
-  LIBOMPTARGET_DEP_LIBELF_INCLUDE_DIRS 
+  LIBOMPTARGET_DEP_LIBELF_INCLUDE_DIRS
   LIBOMPTARGET_DEP_LIBELF_LIBRARIES)
-  
+
 ################################################################################
 # Looking for libffi...
 ################################################################################
@@ -100,15 +101,15 @@ endif()
 
 set(LIBOMPTARGET_DEP_LIBFFI_INCLUDE_DIRS ${LIBOMPTARGET_DEP_LIBFFI_INCLUDE_DIR})
 find_package_handle_standard_args(
-  LIBOMPTARGET_DEP_LIBFFI 
+  LIBOMPTARGET_DEP_LIBFFI
   DEFAULT_MSG
   LIBOMPTARGET_DEP_LIBFFI_LIBRARIES
   LIBOMPTARGET_DEP_LIBFFI_INCLUDE_DIRS)
 
 mark_as_advanced(
-  LIBOMPTARGET_DEP_LIBFFI_INCLUDE_DIRS 
+  LIBOMPTARGET_DEP_LIBFFI_INCLUDE_DIRS
   LIBOMPTARGET_DEP_LIBFFI_LIBRARIES)
-  
+
 ################################################################################
 # Looking for CUDA...
 ################################################################################
@@ -119,6 +120,100 @@ set(LIBOMPTARGET_DEP_CUDA_LIBRARIES ${CUDA_LIBRARIES})
 set(LIBOMPTARGET_DEP_CUDA_INCLUDE_DIRS ${CUDA_INCLUDE_DIRS})
 
 mark_as_advanced(
-  LIBOMPTARGET_DEP_CUDA_FOUND 
+  LIBOMPTARGET_DEP_CUDA_FOUND
   LIBOMPTARGET_DEP_CUDA_INCLUDE_DIRS
   LIBOMPTARGET_DEP_CUDA_LIBRARIES)
+
+################################################################################
+# Looking for Intel HARP...
+################################################################################
+find_path (
+    LIBOMPTARGET_DEP_AALSDK_INCLUDE_DIRS
+  NAMES
+    aalsdk/Runtime.h
+    aalsdk/AALTypes.h
+    aalsdk/Runtime.h
+    aalsdk/AALLoggerExtern.h
+    aalsdk/service/IALIAFU.h
+  PATHS
+    /usr/include
+    /usr/local/include
+    /opt/local/include
+    /sw/include
+    ${AALSDK}/include
+    ENV CPATH)
+
+find_library (
+    LIBOMPTARGET_DEP_AALSDK_AALRT_LIBRARY
+  NAMES
+    aalrt
+  PATHS
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    /sw/lib
+    ${AALSDK}/lib
+    ENV LIBRARY_PATH
+    ENV LD_LIBRARY_PATH)
+
+find_library (
+    LIBOMPTARGET_DEP_AALSDK_AALCLP_LIBRARY
+  NAMES
+    aalclp
+  PATHS
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    /sw/lib
+    ${AALSDK}/lib
+    ENV LIBRARY_PATH
+    ENV LD_LIBRARY_PATH)
+
+find_library (
+    LIBOMPTARGET_DEP_AALSDK_OSAL_LIBRARY
+  NAMES
+    OSAL
+  PATHS
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    /sw/lib
+    ${AALSDK}/lib
+    ENV LIBRARY_PATH
+    ENV LD_LIBRARY_PATH)
+
+find_library (
+    LIBOMPTARGET_DEP_AALSDK_AAS_LIBRARY
+  NAMES
+    AAS
+  PATHS
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    /sw/lib
+    ${AALSDK}/lib
+    ENV LIBRARY_PATH
+    ENV LD_LIBRARY_PATH)
+
+set(LIBOMPTARGET_DEP_AALSDK_LIBRARIES
+  ${LIBOMPTARGET_DEP_AALSDK_LIBRARIES}
+  ${LIBOMPTARGET_DEP_AALSDK_AALCLP_LIBRARY})
+
+set(LIBOMPTARGET_DEP_AALSDK_LIBRARIES
+  ${LIBOMPTARGET_DEP_AALSDK_LIBRARIES}
+  ${LIBOMPTARGET_DEP_AALSDK_OSAL_LIBRARY})
+
+set(LIBOMPTARGET_DEP_AALSDK_LIBRARIES
+  ${LIBOMPTARGET_DEP_AALSDK_LIBRARIES}
+  ${LIBOMPTARGET_DEP_AALSDK_AAS_LIBRARY})
+
+find_package_handle_standard_args(
+  LIBOMPTARGET_DEP_AALSDK
+  DEFAULT_MSG
+  LIBOMPTARGET_DEP_AALSDK_LIBRARIES
+  LIBOMPTARGET_DEP_AALSDK_INCLUDE_DIRS)
+
+mark_as_advanced(
+  LIBOMPTARGET_DEP_AALSDK_INCLUDE_DIRS
+  LIBOMPTARGET_DEP_AALSDK_LIBRARIES)
+
