0db762c54c8a5bf899bde3bb1e532f3aa94f032f
HervÃ© Yviquel <hyviquel@gmail.com>
2017-10-17 14:28:41 -0200

    Use debugLevel for smartnic, similarly than the other plugin

--- libomptarget/plugins/smartnic/src/rtl.cpp
+++ libomptarget/plugins/smartnic/src/rtl.cpp
@@ -34,9 +34,20 @@
 #define TARGET_NAME SMARTNIC
 #endif
 
+#ifdef OMPTARGET_DEBUG
+static int DebugLevel = 0;
+
 #define GETNAME2(name) #name
 #define GETNAME(name) GETNAME2(name)
-#define DP(...) DEBUGP("Target " GETNAME(TARGET_NAME) " RTL", __VA_ARGS__)
+#define DP(...) \
+  do { \
+    if (DebugLevel > 0) { \
+      DEBUGP("Target " GETNAME(TARGET_NAME) " RTL", __VA_ARGS__); \
+    } \
+  } while (false)
+#else // OMPTARGET_DEBUG
+#define DP(...) {}
+#endif // OMPTARGET_DEBUG
 
 #include "../../common/elf_common.c"
 
